582c9dace73227297860826bbc9b91a6e1a10ffc
Karl Schriek <kschriek@gmail.com>
2021-02-22 14:15:59 +0100

    set k8s version = "1.11.1"

--- root.tf
+++ root.tf
@@ -8,7 +8,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "1.9.0"
+      version = "1.11.1"
     }    
     helm = {      
       source  = "hashicorp/helm"
