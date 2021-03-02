43a4a75da86e0d031a6e13c2e65033712762f0c8
Karl Schriek <kschriek@gmail.com>
2021-03-02 21:27:47 +0100

    set external_secrets_deployment_role_arn = var.secret_manager_assume_from_node_role ? module.kubernetes.worker_iam_role_arn : module.external_secrets.external_secrets_role_arn

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {    
 
-    external_secrets_deployment_role_arn = var.secret_manager_assume_from_node_role ? module.external_secrets.external_secrets_role_arn : module.kubernetes.worker_iam_role_arn
+    external_secrets_deployment_role_arn = var.secret_manager_assume_from_node_role ? module.kubernetes.worker_iam_role_arn : module.external_secrets.external_secrets_role_arn
 
 }
